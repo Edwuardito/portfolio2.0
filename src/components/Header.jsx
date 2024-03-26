412614bb10a42612cd563ec48b3e2d5c3e95ba7c
Edwuardito <a20edwin02@gmail.com>
2024-03-26 20:05:40 -0300

    hero complete

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -32,7 +32,7 @@ const Header = () => {
           <nav className={` ${openNavigation ? 'flex' : 'hidden'} fixed top-[5rem] left-0  right-0  bottom-0 bg-n-8 lg:static lg:flex lg:mx-auto lg:bg-transparent`}>
             <div className='relative z-2 flex flex-col items-center justify-center m-auto lg:flex-row'>
               {navigation.map((item) => (
-                <a key={item.id} href={item.url} onClick={handleClick} className={`block realative font-code text-2xl uppercase text-n-1 transition-colors hover:text-color-1 ${item.onlyMobile ? 'lg:hidden' : ''} px-6 py-6 md:py-8 lg:-mr-0.25 lg-text-xs lg:font-semibold ${item.url === pathname.hash ? 'z-2 lg:text-n-1' : 'lg:text-n-1/20'} lg:leading-5 lg:hover:text-n-1 xl:px-12`}>
+                <a key={item.id} href={item.url} onClick={handleClick} className={`block realative font-code text-xl uppercase text-n-1 transition-colors hover:text-color-1 ${item.onlyMobile ? 'lg:hidden' : ''} px-6 py-6 md:py-8 lg:-mr-0.25 lg-text-xs lg:font-semibold ${item.url === pathname.hash ? 'z-2 lg:text-n-1' : 'lg:text-n-1/20'} lg:leading-5 lg:hover:text-n-1 xl:px-12`}>
                   {item.title}
                 </a>
               ))}
